c536cf7a955a4f50827ecf08a2f539f99dc0d0c9
Deforis Nash <defhnash@gmail.com>
2018-08-14 11:46:51 -0500

    Started Styling

--- script.js
+++ script.js
@@ -4,21 +4,55 @@ var App = angular.module('myApp',[]);
 
 App.controller('control',['$scope','$http',function($scope,$http){
 
-$scope.boxes = function() {
 
-  arr = [];
-  console.log($scope.number);
-  var x = $scope.number;
-  for (var i = 0; i < x; i++) {
-    var number = {
-      val : i + 1
-    };
-    arr.push(number);
-    console.log(arr);
-  }
-  $scope.disp = arr;
+  atk = [
+    {name:"Sledge"},
+    {name:"Thatcher"},
+    {name:"Ash"},
+    {name:"Thermite"},
+    {name:"Twitch"},
+    {name:"Montagne"},
+    {name:"Glaz"},
+    {name:"Fuze"},
+    {name:"Blitz"},
+    {name:"IQ"},
+    {name:"Buck"},
+    {name:"BlackBeard"},
+    {name:"Capitao"},
+    {name:"Hibana"},
+    {name:"Jackal"},
+    {name:"Ying"},
+    {name:"Zofia"},
+    {name:"Dokkaebi"},
+    {name:"Lion"},
+    {name:"Finka"}
+  ];
+  def = [
+    {name:"Smoke"},
+    {name:"Mute"},
+    {name:"Castle"},
+    {name:"Pulse"},
+    {name:"Doc"},
+    {name:"Rook"},
+    {name:"Kapkan"},
+    {name:"Tachanka"},
+    {name:"Jager"},
+    {name:"Bandit"},
+    {name:"Frost"},
+    {name:"Valkyrie"},
+    {name:"Caveira"},
+    {name:"Echo"},
+    {name:"Mira"},
+    {name:"Lesion"},
+    {name:"Ela"},
+    {name:"Vigil"},
+    {name:"Maestro"},
+    {name:"Alibi"}
+  ];
+
+  $scope.dispa = atk;
+  $scope.dispb = def;
 
 
-};
 
 }]);
